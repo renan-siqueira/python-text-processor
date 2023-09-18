1420327fb0b43a0030193408c5277d5841d801d8
Renan Siqueira Antonio <renan.siqu@gmail.com>
2023-09-18 09:54:12 -0300

    feat: Criação de testes para sumarização e detecção de idioma. Criação de funcionalidade para contar caracteres e palavras

--- modules/analyzer.py
+++ modules/analyzer.py
@@ -1,5 +1,7 @@
-from collections import Counter
 import re
+
+from collections import Counter
+from langdetect import detect
 
 
 def word_count(text):
@@ -17,3 +19,29 @@ def word_frequency(text):
 def find_matches(text, pattern):
     """Retorna todas as correspondências de um padrão regex no texto."""
     return re.findall(pattern, text)
+
+
+def summarize_text(text):
+    sentences = text.split('.')
+    summary = '.'.join(sentences[:3])
+    return summary
+
+
+def detect_language(text):
+    return detect(text)
+
+
+def count_words(text):
+    return len(text.split())
+
+
+def count_characters_with_spaces(text):
+    return len(text)
+
+
+def count_characters_without_spaces(text):
+    return len(text.replace(" ", ""))
+
+
+def count_paragraphs(text):
+    return len([p for p in text.split("\n\n") if p])
--- tests/test_analyzer.py
+++ tests/test_analyzer.py
@@ -1,5 +1,5 @@
 import unittest
-from modules.analyzer import word_count, word_frequency
+from modules.analyzer import word_count, word_frequency, summarize_text, detect_language
 
 
 class TestAnalyzer(unittest.TestCase):
@@ -13,6 +13,24 @@ class TestAnalyzer(unittest.TestCase):
     def test_word_frequency(self):
         freq = word_frequency(self.text)
         self.assertEqual(freq, {"hello": 2, "world": 1, "universe": 1})
+
+    def test_summarize_text(self):
+        text = "Esta é a primeira frase. Esta é a segunda frase. Esta é a terceira frase. Esta é a quarta frase."
+        expected_summary = "Esta é a primeira frase. Esta é a segunda frase. Esta é a terceira frase"
+        result = summarize_text(text)
+        self.assertEqual(result, expected_summary)
+    
+    def test_detect_language_english(self):
+        text = "This is a simple English text."
+        expected_language = "en"
+        result = detect_language(text)
+        self.assertEqual(result, expected_language)
+    
+    def test_detect_language_portuguese(self):
+        text = "Este é um texto simples em português."
+        expected_language = "pt"
+        result = detect_language(text)
+        self.assertEqual(result, expected_language)
 
 
 if __name__ == "__main__":
